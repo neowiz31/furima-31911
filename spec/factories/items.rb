62b1e61e90e86132ccf9ae50bc500579f79596d6
neowiz31 <neowiz23@gmail.com>
2021-01-28 18:31:50 +0900

    itemモデル単体テストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name          { 'テスト' }
+    item_info          { '商品情報' }
+    price              { '300' }
+    delivery_cost_id   { '2' }
+    delivery_time_id   { '2' }
+    item_category_id   { '2' }
+    item_quality_id    { '2' }
+    prefecture_id      { '2' }
+    association :user
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/furima-intro05.png'), filename: 'furima-intro05.png')
+    end
   end
 end

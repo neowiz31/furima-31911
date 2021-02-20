36ebb0b6c9dc73eee595d3b2cf859d458d294571
neowiz31 <neowiz23@gmail.com>
2021-02-20 13:52:32 +0900

    商品購入機能モデル単体テストコード作成

--- spec/factories/item_orders.rb
+++ spec/factories/item_orders.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item_order do
-    
+    postal_code          {'000-0000'}
+    prefecture_id        {2}
+    city                 {'渋谷区'}
+    house_number         {'渋谷町6-10'}
+    phone_number         {'08012345678'}
+    item_id              {1}
+    user_id              {1}
+    token                {0}
   end
 end
--- spec/models/item_order_spec.rb
+++ spec/models/item_order_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe ItemOrder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe ItemOrder do
+  before do
+    @item_order = FactoryBot.build(:item_order)
+  end
+
+  describe '商品購入手続き' do
+    context '商品購入が成功する時' do
+      it "建物名を除く全ての項目が入力されていれば登録できる" do
+        expect(@item_order).to be_valid
+      end
+    end
+
+    context '商品購入が失敗する時' do
+      it 'postal_codeが存在しなければ登録できない' do
+        @item_order.postal_code = ""
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid. Include hyphen(-)")
+      end
+
+      it 'postal_codeに「-」が無ければ登録できない' do
+        @item_order.postal_code = "1234567"
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
+      it '都道府県が「---」では登録できない' do
+        @item_order.prefecture_id = 1
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include('Prefecture must be other than 1')
+      end
+
+      it '市区町村が存在しなければ登録できない' do
+        @item_order.city = ""
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '番地が存在しなければ登録できない' do
+        @item_order.house_number = ""
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("House number can't be blank")
+      end
+
+      it '電話番号が存在しなければ登録できない' do
+        @item_order.phone_number = ""
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '電話番号が12桁を超えると登録できない' do
+        @item_order.phone_number = "080123456789"
+        @item_order.valid?
+        expect(@item_order.errors.full_messages).to include("Phone number is invalid")
+      end
+    end
+  end
 end
